d3e0219b1ccb1341a1b0ec79bd1f6da3d8983c17
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-10-08 17:19:56 +0800

    feat(react): ✨ hook rule

--- packages/eslint/react/index.js
+++ packages/eslint/react/index.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
-  extends: ['plugin:react/recommended', '@minko-fe/eslint-config-ts'],
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', '@minko-fe/eslint-config-ts'],
+  settings: {
+    react: {
+      version: '17.0',
+    },
+  },
   rules: {
     'jsx-quotes': ['error', 'prefer-single'],
     'react/react-in-jsx-scope': 'off',
