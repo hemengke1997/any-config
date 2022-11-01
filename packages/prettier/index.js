f20ba12b33ad0c712c03f243845d5fadaaf8b004
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-11-01 14:36:35 +0800

    revert(prettier): crlf to auto

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -3,7 +3,7 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   tabWidth: 2,
-  endOfLine: 'crlf',
+  endOfLine: 'auto',
   printWidth: 120,
   semi: false,
   jsxSingleQuote: true,
