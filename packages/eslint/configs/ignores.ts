34b190c5abe242ef2cca91e0995c589742a27382
hemengke1997 <23536175@qq.com>
2023-09-26 18:22:18 +0800

    feat(eslint): add gitignore plugin

--- packages/eslint/configs/ignores.ts
+++ packages/eslint/configs/ignores.ts
@@ -1,4 +1,5 @@
 import { type FlatESLintConfigItem } from 'eslint-define-config'
+import gitignore from 'eslint-config-flat-gitignore'
 import { GLOB_EXCLUDE } from '../globs'
 
-export const ignores: FlatESLintConfigItem[] = [{ ignores: GLOB_EXCLUDE }]
+export const ignores: FlatESLintConfigItem[] = [{ ignores: [...GLOB_EXCLUDE, ...gitignore().ignores] }]
