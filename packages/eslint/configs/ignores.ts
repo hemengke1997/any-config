76db9e3868f2d31deab611f115d32f02b6d15be9
hemengke1997 <23536175@qq.com>
2023-09-26 18:38:47 +0800

    feat(eslint): support gitignore

--- packages/eslint/configs/ignores.ts
+++ packages/eslint/configs/ignores.ts
@@ -1,5 +1,39 @@
+import fs from 'node:fs'
 import { type FlatESLintConfigItem } from 'eslint-define-config'
-import gitignore from 'eslint-config-flat-gitignore'
+// @ts-expect-error missing types
+import parse from 'parse-gitignore'
+import findUp from 'find-up'
 import { GLOB_EXCLUDE } from '../globs'
+
+interface FlatGitignoreOptions {
+  files?: string | string[]
+}
+
+interface FlatConfigItem {
+  ignores: string[]
+}
+
+function gitignore(options: FlatGitignoreOptions = {}): FlatConfigItem {
+  const ignores: string[] = []
+
+  const { files: _files = '.gitignore' } = options
+  const files = Array.isArray(_files) ? _files : [_files]
+
+  for (const file of files) {
+    const _file = findUp.sync(file)
+    if (!_file) continue
+    const content = fs.readFileSync(_file, 'utf8')
+    const parsed = parse(content)
+    const globs = parsed.globs()
+    for (const glob of globs) {
+      if (glob.type === 'ignore') ignores.push(...glob.patterns)
+      else if (glob.type === 'unignore') ignores.push(...glob.patterns.map((pattern: string) => `!${pattern}`))
+    }
+  }
+
+  return {
+    ignores,
+  }
+}
 
 export const ignores: FlatESLintConfigItem[] = [{ ignores: [...GLOB_EXCLUDE, ...gitignore().ignores] }]
