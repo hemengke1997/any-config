ab0abd569a18c49d0dc5c2756807607556a257de
hemengke <23536175@qq.com>
2024-02-05 11:58:38 +0800

    fix(eslint): tailwind plugin peer dependency warning
    release

--- packages/eslint/CHANGELOG.md
+++ packages/eslint/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @minko-fe/eslint-config
 
+## 2.1.1
+
+### Patch Changes
+
+- try to fix tailwind plugin peer dependency warning
+
 ## 2.1.0
 
 ### Minor Changes
--- packages/eslint/configs/tailwindcss.ts
+++ packages/eslint/configs/tailwindcss.ts
@@ -1,17 +1,17 @@
 import { type FlatESLintConfigItem } from 'eslint-define-config'
 // @ts-expect-error missing types
-import * as pluginTailwindcss from 'eslint-plugin-tailwindcss'
-// @ts-expect-error missing types
 import * as pluginTailwindcss3_11 from 'eslint-plugin-tailwindcss-3.11.0'
 // @ts-expect-error missing types
 import * as pluginTailwindcss3_13_1 from 'eslint-plugin-tailwindcss-3.13.1'
+// @ts-expect-error missing types
+import * as pluginTailwindcss3_14_1 from 'eslint-plugin-tailwindcss-3.14.1'
 import { getPackageInfoSync } from 'local-pkg'
 import semver from 'semver'
 import { GLOB_JSX, GLOB_TSX, GLOB_VUE } from '../globs'
 import { interopDefault } from '../utils'
 
 const EslintTailwindVersionMap = new Map([
-  ['^3.4.0', pluginTailwindcss],
+  ['^3.4.0', pluginTailwindcss3_14_1],
   ['^3.3.2', pluginTailwindcss3_13_1],
   ['^3.2.2', pluginTailwindcss3_11],
 ])
@@ -28,8 +28,12 @@ function resolveRightTwVersion() {
       }
     }
   }
-  return interopDefault(pluginTailwindcss)
+
+  const [first] = EslintTailwindVersionMap.keys()
+  return interopDefault(EslintTailwindVersionMap.get(first))
 }
+
+const pluginTailwindcss = resolveRightTwVersion()
 
 export const tailwindcss: FlatESLintConfigItem[] = [
   {
